02cfe91eb3f9697787460ad77d73ff951f801f50
John Törnblom <john.tornblom@gmail.com>
2026-09-13 19:02:32 +0200

    uri_get_filename: sanity check uri

--- uri.c
+++ uri.c
@@ -336,9 +336,13 @@ uri_get_path(const char* uri) {
 
 char*
 uri_get_filename(const char* uri) {
-  size_t len = strlen(uri);
   const char* begin = 0;
   const char* end = 0;
+  size_t len;
+
+  if(!(len=strlen(uri))) {
+    return 0;
+  }
 
   end = uri + len;
   for(int i=0; i<len; i++) {
